4d557c38ce30faa08aa0136f7bfbb27121aa0ca1
gaston fuentes <gastonfuentes1991@hotmail.com>
2020-05-08 21:53:56 -0300

    dal 8/5/20 conexion a la bd

---
+++ comoEnCasa/comoEnCasa.DAL.SQLSERVER/DBSqlServ.cs
@@ -0,0 +1,86 @@
+﻿using comoEnCasa.COMMON.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+
+namespace comoEnCasa.DAL.SQLSERVER
+{
+    class DBSqlServ : IDB
+    {
+        private SqlConnection conexion;
+
+        public DBSqlServ()
+        {
+            string server = "";
+            string database = "";
+            string uid = "";
+            string password = "";
+            conexion = new SqlConnection(string.Format("SERVER={0};DATABASE={1}; UID={2};PASSWORD={3};SslMode= none;", server, database, uid, password));
+            Conectar();
+        }
+
+        private bool Conectar()
+        {
+            try
+            {
+                conexion.Open();
+                Error = "";
+                return true;
+
+            }
+            catch(SqlException ex)
+            {
+                Error = ex.Message;
+                return false;
+            }
+        }
+
+        public string Error {get; private set;}
+
+        public bool Comando(string command)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand(command, conexion);
+                cmd.ExecuteNonQuery();
+                Error = "";
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Error = ex.Message;
+                return false;                
+            }
+        }
+
+        public object Consulta(string consulta)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand(consulta, conexion);
+                SqlDataReader dr = cmd.ExecuteReader();
+                Error = "";
+                return dr;
+            }
+            catch (Exception ex)
+            {
+                Error = ex.Message;
+                return null;
+
+            }
+        }
+        ~DBSqlServ()
+        {
+            if (conexion.State == System.Data.ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
---
+++ comoEnCasa/comoEnCasa.DAL.SQLSERVER/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using comoEnCasa.COMMON.Entidades;
+using comoEnCasa.COMMON.Interfaces;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace comoEnCasa.DAL.SQLSERVER
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseDTO
+    {
+
+        private DBSqlServ db;
+        private bool idEsAutonumerico;
+        private AbstractValidator<T> validator;
+        public GenericRepository(AbstractValidator<T> validator, bool idEsAutonumerico = true)
+        {
+            this.validator = validator;
+            this.idEsAutonumerico = idEsAutonumerico;
+            db = new DBSqlServ();
+        }
+
+        public string Error { get; private set; }
+
+        public IEnumerable<T> Read => throw new NotImplementedException();
+
+        public bool Create(T entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Query(Expression<Func<T, bool>> predicado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T SeachById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T endidad)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
